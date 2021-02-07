c5abc35ea036c41a7192474193e88dc20285d227
moaztaha <moaaztaha7@gmal.com>
2021-02-07 11:57:46 +0200

    web working

--- web-app/app/server.py
+++ web-app/app/server.py
@@ -1,18 +1,33 @@
 from starlette.applications import Starlette
 from starlette.responses import HTMLResponse
 from starlette.staticfiles import StaticFiles
+from starlette.templating import Jinja2Templates
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn, aiohttp, asyncio
 from io import BytesIO
 from fastai.vision.all import *
+import math
 
 model_file_url = 'https://github.com/psmathur/pets_breeds_classifier/blob/master/pets_res_34_export.pkl?raw=true'
 model_file_name = 'export.pkl'
 path = Path(__file__).parent
 
+templates = Jinja2Templates(directory='app/templates')
+
 app = Starlette()
 app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
+
+api_key = 'AIzaSyBOp0pH8QYUOc1E0CbHU8a9_N2Dk0JmJBU'
+# url variable store url 
+url = "https://maps.googleapis.com/maps/api/place/textsearch/json?"
+
+dict = {}
+file = open("message.txt")
+for line in file:
+  key, value = line.split()
+  dict[int(key)] = value
+
 
 # async def download_file(url, dest):
 #     if dest.exists(): return
@@ -45,7 +60,9 @@ PREDICTION_FILE_SRC = path/'static'/'predictions.txt'
 async def upload(request):
     form = await request.form()
     img_bytes = await (form["file"].read())
-    return predict_from_bytes(img_bytes)
+    output, cords = predict_from_bytes(img_bytes)
+    return templates.TemplateResponse('result.html', {'request': request, 'output': output, 'cords': cords})
+
 
 def predict_from_bytes(img_bytes):
     pred,pred_idx,probs = learn.predict(img_bytes)
@@ -54,8 +71,29 @@ def predict_from_bytes(img_bytes):
     result_html1 = path/'static'/'result1.html'
     result_html2 = path/'static'/'result2.html'
     
-    result_html = str(result_html1.open().read() +str(predictions[0:3]) + result_html2.open().read())
-    return HTMLResponse(result_html)
+    output = ''
+    for pred in predictions[0:3]:
+        output+= dict[pred[0]] +  ' ' + str(float("{:.3f}".format(pred[1]))) + ' |\t'
+    
+    print(dict[predictions[0][0]])
+    search_output = search(dict[predictions[0][0]])
+        
+
+
+    result_html = str(result_html1.open().read() + output + result_html2.open().read())
+    # return HTMLResponse(result_html)
+    return output, search_output
+
+def search(prediction):
+    url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input={prediction}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBOp0pH8QYUOc1E0CbHU8a9_N2Dk0JmJBU"
+    #url = f"https://maps.googleapis.com/maps/api/place/findplacefromtext/json?query={prediction}&inputtype=textquery&fields=formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBOp0pH8QYUOc1E0CbHU8a9_N2Dk0JmJBU"
+
+    r = requests.get(url)
+    x = r.json()
+    lat, lng = x['candidates'][0]['geometry']['location']['lat'], x['candidates'][0]['geometry']['location']['lng']
+    return { 'lat': lat, 'lng': lng }
+    
+
 
 @app.route("/")
 def form(request):
